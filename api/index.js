24a247d44038aaf88e5c860f8e3dc600a06a37c7
riddhirewatkar <riddhirewatkar10@gmail.com>
2023-04-10 21:34:47 +0530

    in progess

---
+++ api/index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const Post = require('./models/post');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/'});
+const fs = require('fs');
+
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+
+app.use(cors({credentials: true, origin:'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose
+    .connect('mongodb+srv://blog:7GnwAAznhi3AsVH4@cluster0.aicefkk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(5000, () => {
+            console.log("Server Running on Port 4000");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.post('/register', async (req, res) =>{
+    const {username, password} = req.body;
+    try{
+        const userDoc = await User.create({
+            username, 
+            password: bcrypt.hashSync(password, salt),  
+        });
+        res.json(userDoc);
+    }
+    catch(e){
+        res.status(400).json(e);
+    }
+});
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    if (userDoc) {
+        const passOk = await bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+                if (err) throw err;
+                res.cookie('token', token).json({
+                    id: userDoc._id,
+                    username,
+                });
+            });
+        } else {
+            res.status(400).json('wrong credentials');
+        }
+    } else {
+        res.status(400).json('user not found');
+    }
+});
+
+app.get('/profile', (req, res) =>{
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) =>{
+        if(err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const {originalname, path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+
+    const {title, summary, content} = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath,
+    });
+    res.json(postDoc);
+});
+
+app.get('/post', async (req, res) =>{
+    res.json(await Post.find());
+});
+
+app.listen(4000, () => {
+    console.log("Server Running on Port 4000");
+});
